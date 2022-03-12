54b11fb09dda4808f1edb3635588597819787f79
Gilevich Bogdan <88113778+el1ght@users.noreply.github.com>
2022-03-12 19:17:07 +0200

    experimental video js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,137 @@
-(()=>{for(var e=document.images,t=e.length,o=0,a=document.getElementById("page-preloader"),n=document.getElementById("load_perc"),r=0;r<t;r++)image_clone=new Image,image_clone.onload=s,image_clone.onerror=s,image_clone.src=e[r].src;function s(){o++,n.innerHTML=(100/t*o<<0)+"%",o>=t&&setTimeout((function(){a.classList.contains("done")||a.classList.add("done")}),1e3)}TweenMax.to("#t",.3,{"stroke-dashoffset":-16,ease:Linear.easeNone,repeat:-1,yoyo:!1,repeatDelay:0}),TweenMax.to("#g",.2,{"stroke-dashoffset":-16,ease:Linear.easeNone,repeat:-1,yoyo:!1,repeatDelay:0}),TweenMax.to("#r",.4,{"stroke-dashoffset":-21,ease:Linear.easeNone,repeat:-1,yoyo:!1,repeatDelay:0}),TweenMax.to("#l",.5,{"stroke-dashoffset":-18,ease:Linear.easeNone,repeat:-1,yoyo:!1,repeatDelay:0}),TweenMax.to("#p",.2,{"stroke-dashoffset":-17,ease:Linear.easeNone,repeat:-1,yoyo:!1,repeatDelay:0}),TweenMax.to("#x",.1,{"stroke-dashoffset":-18,ease:Linear.easeNone,repeat:-1,yoyo:!1,repeatDelay:0}),VanillaTilt.init(document.querySelectorAll(".first-layer"),{max:10,speed:200,reverse:!0});var i=document.querySelector(".menu__icon");if(i){var l=document.querySelector(".nav");i.addEventListener("click",(function(e){document.body.classList.toggle("_lock"),i.classList.toggle("_active"),l.classList.toggle("_active")}))}new Swiper(".swiper-container",{loop:!0,direction:"vertical",spaceBetween:30,centeredSlides:!0,speed:500,autoplay:{delay:2e3},pagination:{el:".swiper-pagination",type:"progressbar"}}),new Swiper(".swiper-container-2",{loop:!0,direction:"vertical",spaceBetween:30,centeredSlides:!0,speed:500,autoplay:{delay:2e3},pagination:{el:".swiper-pagination-2",type:"progressbar"}});var c=document.querySelector(".intro"),d=c.querySelector("video"),g=document.getElementsByClassName("content"),y=document.getElementsByClassName("content2"),p=(document.querySelector("section"),document.getElementsByClassName("content"),new ScrollMagic.Controller),m=new ScrollMagic.Scene({duration:7e3,triggerElement:c,triggerHook:0}).setPin(c).addTo(p),u=TweenMax.fromTo(g,3,{y:0},{y:-2e3}),w=(new ScrollMagic.Scene({duration:7e3,triggerElement:c,triggerHook:0}).setTween(u).addTo(p),TweenMax.fromTo(y,.5,{y:-7e3},{y:0})),f=(new ScrollMagic.Scene({duration:7e3,triggerElement:c,triggerHook:0}).setTween(w).addTo(p),0),T=0;m.on("update",(function(e){f=e.scrollPos/1e3})),setInterval((function(){T+=.2*(f-T),d.currentTime=T}),70),AOS.init()})();
+let
+  images              = document.images,
+  images_total_count  = images.length,
+  images_loaded_count = 0,
+  preloader           = document.getElementById('page-preloader'),
+  perc_display        = document.getElementById('load_perc');
+
+for( let i = 0; i < images_total_count; i++ ){
+  image_clone         = new Image();
+  image_clone.onload  = image_loaded;
+  image_clone.onerror = image_loaded;
+  image_clone.src     = images[i].src;
+}
+
+function image_loaded() {
+  images_loaded_count++;
+  perc_display.innerHTML = (( (100 / images_total_count) * images_loaded_count ) << 0) + '%';
+
+  if( images_loaded_count >= images_total_count ){
+    setTimeout(function() {
+      if( !preloader.classList.contains('done') ){
+        preloader.classList.add('done');
+      }
+    }, 1000);
+  }
+}
+
+
+TweenMax.to('#t', 0.3, { 'stroke-dashoffset':  -16, ease:Linear.easeNone, repeat: -1, yoyo: false, repeatDelay: 0 })
+TweenMax.to('#g', 0.2, { 'stroke-dashoffset':  -16, ease:Linear.easeNone, repeat: -1, yoyo: false, repeatDelay: 0 })
+TweenMax.to('#r', 0.4, { 'stroke-dashoffset':  -21, ease:Linear.easeNone, repeat: -1, yoyo: false, repeatDelay: 0 })
+TweenMax.to('#l', 0.5, { 'stroke-dashoffset':  -18, ease:Linear.easeNone, repeat: -1, yoyo: false, repeatDelay: 0 })
+TweenMax.to('#p', 0.2, { 'stroke-dashoffset':  -17, ease:Linear.easeNone, repeat: -1, yoyo: false, repeatDelay: 0 })
+TweenMax.to('#x', 0.1, { 'stroke-dashoffset':  -18, ease:Linear.easeNone, repeat: -1, yoyo: false, repeatDelay: 0 })
+
+VanillaTilt.init(document.querySelectorAll(".first-layer"), {
+  max: 10,
+  speed: 200,
+  reverse: true
+});
+
+
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+  const menuBody = document.querySelector('.nav');
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  })
+}
+
+
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  direction: "vertical",
+  
+  spaceBetween: 30,
+  centeredSlides: true,
+  speed: 500,
+  mousewheel: {
+    invert: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar"
+  },
+});
+
+
+
+document.getElementById("hello").setAttribute("autoplay", "autoplay");
+
+
+
+
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = document.getElementsByClassName("content");
+const text2 = document.getElementsByClassName("content2");
+//END SECTION
+const section = document.querySelector("section");
+const end = document.getElementsByClassName("content");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 7000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setPin(intro)
+  .addTo(controller);
+
+
+
+const textAnim = TweenMax.fromTo(text, 3, { y: 0 }, { y: -2000 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 7000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+const textAnim2 = TweenMax.fromTo(text2, 0.5, { y: -2000 }, { y: 150 });
+
+let scene3 = new ScrollMagic.Scene({
+  duration: 7000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim2)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.2;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+
+
+  video.currentTime = delay;
+}, 70);
+
+
+
+AOS.init();
